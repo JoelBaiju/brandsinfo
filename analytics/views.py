fd5aa65c8942b2ee118b2dbd4fc73c4276f29d41
JoelBaiju <joelbaiju98@gmail.com>
2025-04-28 11:35:20 +0530

    first commit

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import RequestLog
+import json
 
-# Create your views here.
+@csrf_exempt
+def track_visit(request):
+    if request.method == 'POST':
+        ip_address = get_client_ip(request)
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+        accept_language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')
+        referer = request.META.get('HTTP_REFERER', '')
+        origin = request.META.get('HTTP_ORIGIN', '')
+        
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            data = {}
+
+        path = data.get('path', '')
+        extra = data.get('extra', '')
+
+        # Check if IP already exists
+        if RequestLog.objects.filter(ip_address=ip_address, path=path, method='VISIT').exists():
+            return JsonResponse({'status': 'already_tracked'})
+
+        # Save if not already tracked
+        RequestLog.objects.create(
+            ip_address=ip_address,
+            user_agent=user_agent,
+            accept_language=accept_language,
+            referer=referer,
+            origin=origin,
+            method='VISIT',  # Custom method to distinguish
+            path=path,
+            query_params=extra,
+        )
+
+        return JsonResponse({'status': 'ok'})
+    
+    return JsonResponse({'error': 'invalid method'}, status=400)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- brandsinfo/urls.py
+++ brandsinfo/urls.py
@@ -12,12 +12,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from usershome.Views import payment_view
+import analytics.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/',include(usershome.urls)),
     path('badmin/',include(bAdmin.urls)),
     path('communications/',include(communications.urls)),
+    path('analytics/',include(analytics.urls)),
     path('mapper/<int:maping_id>/',mapper.mapper_view),
     
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
