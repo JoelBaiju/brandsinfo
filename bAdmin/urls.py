e37523b944995096ca3780e5bb21af7a7449c6bd
JoelBaiju <joelbaiju98@gmail.com>
2025-05-03 21:45:13 +0530

    first commit

--- bAdmin/urls.py
+++ bAdmin/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     
     path('login/',auth_views.admin_auth_view),
     path('dash/',views.admin_dashboard_view),   
-    path('add-general-cats/', views.AddGeneralCatsView.as_view(), name='add-general-cats'),
-    path('add-descriptive-cats/', views.AddDescriptiveCatsView.as_view(), name='add-descriptive-cats'),
+    path('add_general_cats/', views.AddGeneralCatsView.as_view(), name='add-general-cats'),
+    path('add_descriptive_cats/', views.AddDescriptiveCatsView.as_view(), name='add-descriptive-cats'),
     path('get_gcats/',views.GetAllGcats.as_view()),
     path('get_dcats/',views.GetAllDcats.as_view()),
     path('edit_dcats/<int:pk>/',views.EditDcats.as_view()),
