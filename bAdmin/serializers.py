24d8a7a1aa9c905c6eb2a5a0f0cf7967386728b1
JoelBaiju <joelbaiju98@gmail.com>
2025-05-13 14:58:46 +0530

    first commit

--- bAdmin/serializers.py
+++ bAdmin/serializers.py
@@ -57,9 +57,12 @@ class ProductSubCatsSerializer(serializers.ModelSerializer):
 class BuisnessesAdminlistSerializer(serializers.ModelSerializer):
     plan = serializers.SerializerMethodField()
     image = serializers.ImageField()
+    city = serializers.SerializerMethodField()
+    locality = serializers.SerializerMethodField()
     class Meta:
         model = Buisnesses
         fields = ['id', 'name', 'building_name','locality', 'city', 'state', 'verified', 'created_on', 'assured', 'plan' , 'rating','image' , 'buisness_type']
     
     def get_plan(self, obj):
         return obj.plan.plan_name if obj.plan else None
+    
