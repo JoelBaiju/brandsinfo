531170c7965a5d30d7c797944665c3a19fcc465a
joel <joelbaiju98@gmail.com>
2025-07-01 03:01:44 -0700

    hello

--- brandsinfo/settings.py
+++ brandsinfo/settings.py
@@ -393,7 +393,7 @@ AI_METADATA_PRIORITY = [
 
 
 REVIEW_AI_PRIORITY = [
-    # "gpt",         # First priority
+    "gpt",         # First priority
     "cohere"  # Fallback
     # "sarvam" # Add if needed later
 ]
